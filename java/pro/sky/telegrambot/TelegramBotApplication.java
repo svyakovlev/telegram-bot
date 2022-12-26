22b6a8a76f0019d650e9ac26bd69d3c8f9f4036a
Сергей <s.v.yakovlev@yandex.ru>
2022-12-26 23:07:59 +0400

    всё работает

--- java/pro/sky/telegrambot/TelegramBotApplication.java
+++ java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -42,6 +42,5 @@ public class TelegramBotApplication {
             SendMessage replyMessage = new SendMessage(chatId, notificationMessage);
             telegramBot.execute(replyMessage);
         }
-
     }
 }
